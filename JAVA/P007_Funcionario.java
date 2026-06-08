6c562ac5e57b8f324dfc602fd44bb029bf2a4d0d
Davi Inácio <dviinacioo@gmail.com>
2026-06-08 10:53:12 -0300

    Add files via upload

---
+++ JAVA/P007_Funcionario.java
@@ -0,0 +1,21 @@
+public class P007_Funcionario {
+
+    private String matricula;
+    private String nome;
+    private boolean ativo;
+
+    public P007_Funcionario(String matricula, String nome, boolean ativo){
+        this.matricula = matricula;
+        this.nome = nome;
+        this.ativo = ativo;
+    }
+
+    public void exibirDados(){
+        System.out.println("Matricula: " + matricula);
+        System.out.println("Nome: " + nome);
+    }
+
+    public boolean trabalhoLiberado(){
+        return matricula.length() >= 6 && ativo;
+    }
+}
---
+++ JAVA/P007_Main.java
@@ -0,0 +1,14 @@
+public class P007_Main {
+    
+    public static void main(String[] args) {
+    P007_Funcionario funcionario1 = new P007_Funcionario("trabalhador01", "João", true);
+    P007_Funcionario funcionario2 = new P007_Funcionario("trabalhador02", "Gustavo", true);
+
+    funcionario1.exibirDados();
+    System.out.println("Acesso pertmitido: " + funcionario1.trabalhoLiberado());
+
+    funcionario2.exibirDados();
+    System.out.println("Acesso pertmitido: " + funcionario2.trabalhoLiberado());
+    }
+
+}
