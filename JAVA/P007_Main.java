86f2f8e57bcb97c2cba0b50dba891b9a193da7c2
Davi Inácio <dviinacioo@gmail.com>
2026-06-08 11:09:06 -0300

    Refactor P007_Main to fix variable name and add spacing

--- JAVA/P007_Main.java
+++ JAVA/P007_Main.java
@@ -2,10 +2,11 @@ public class P007_Main {
     
     public static void main(String[] args) {
     P007_Funcionario funcionario1 = new P007_Funcionario("trabalhador01", "João", true);
-    P007_Funcionario funcionario2 = new P007_Funcionario("trabalhador02", "Gustavo", true);
+    P007_Funcionario funcionario2 = new P007_Funcionario("traba", "Gustavo", true);
 
     funcionario1.exibirDados();
     System.out.println("Acesso pertmitido: " + funcionario1.trabalhoLiberado());
+    System.out.println();
 
     funcionario2.exibirDados();
     System.out.println("Acesso pertmitido: " + funcionario2.trabalhoLiberado());
