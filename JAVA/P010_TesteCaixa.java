3bd56866a6aeedcb026aafb434767d692bdd7945
Davi Inácio <dviinacioo@gmail.com>
2026-06-12 11:16:06 -0300

    Add files via upload

---
+++ JAVA/P010_TesteCaixa.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class P010_TesteCaixa {
+    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,
+            "Olá Mundo!");
+    }
+}
